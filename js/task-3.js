f40515a99976d51372745b0f8f49c216590129c5
Nippondanso <mkopaev@gmail.com>
2024-10-13 20:54:56 +0300

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,23 @@
+// Напиши скрипт, який під час набору тексту в інпуті input#name-input (подія input) підставляє його поточне значення в span#name-output як ім’я для привітання.
+//  Обов’язково очищай значення в інпуті по краях від пробілів . Якщо інпут порожній або містить лише пробіли, то замість імені у спан має підставлятися рядок "Anonymous".
+
+// <input type="text" id="name-input" placeholder="Please enter your name" />
+// <h1>Hello, <span id="name-output">Anonymous</span>!</h1>
+
+// На що буде звертати увагу ментор при перевірці:
+// На елементі input#name-input прослуховується подія input
+// Під час набору тексту в інпуті його поточне значення підставляється в span#name-output як ім’я для привітання
+// Значення в інпуті очищене від пробілів по краях
+// Якщо інпут порожній або містить лише пробіли, то замість імені у спан має підставлятися рядок "Anonymous"
+
+'use strinct';
+
+const input = document.querySelector('#name-input'),
+    output = document.querySelector('#name-output');
+
+input.classList.add('input__style');
+
+input.addEventListener('input', e => {
+    let inputValue = String(e.target.value).trim();
+    output.textContent = inputValue ? inputValue : 'Anonymous';
+})
